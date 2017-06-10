15e42d5da96074a1cb1c3b18b06ba6d20bb3d83e
Paul Baker <paul.baker@wgu.edu>
2017-06-10 13:58:17 -0600

    Ensuring that our code works and fails correctly when we try to load the headers with our data

--- src/test/java/wtf/paulbaker/myai/DataSetTest.java
+++ src/test/java/wtf/paulbaker/myai/DataSetTest.java
@@ -10,6 +10,7 @@ import java.net.URISyntaxException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Created by paul.baker on 6/10/17.
@@ -19,8 +20,7 @@ public class DataSetTest {
     @Test
     public void testXORLoads() throws URISyntaxException, IOException {
         File xorFile = FileUtils.fromClassPath("dataset/xor.csv");
-
-        DataSet xorData = DataSet.fromCSVFile(xorFile, 2);
+        DataSet xorData = DataSet.fromCSVFile(xorFile, 2, true);
 
         assertThat(xorData.getInput(0), is(new double[]{0, 0}));
         assertThat(xorData.getInput(1), is(new double[]{0, 1}));
@@ -31,5 +31,12 @@ public class DataSetTest {
         assertThat(xorData.getOutput(1), is(new double[]{1}));
         assertThat(xorData.getOutput(2), is(new double[]{1}));
         assertThat(xorData.getOutput(3), is(new double[]{0}));
+    }
+
+    @Test
+    public void testXORFailsToLoadWithoutLineSkip() throws IOException {
+        File xorFile = FileUtils.fromClassPath("dataset/xor.csv");
+
+        assertThrows(NumberFormatException.class, () -> DataSet.fromCSVFile(xorFile, 2, false));
     }
 }
